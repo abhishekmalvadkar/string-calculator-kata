3c68e12f24b7b578a9a059bbbab7ffa323a30d15
Abhishek <abhi@dev.com>
2025-10-19 20:26:48 +0530

    GH-2 : Implemented Requirement #1

--- src/main/java/com/amalvadkar/sck/StringCalculator.java
+++ src/main/java/com/amalvadkar/sck/StringCalculator.java
@@ -2,6 +2,10 @@ package com.amalvadkar.sck;
 
 public class StringCalculator {
     public int execute(String input) {
+        if (input.contains("+")) {
+            String[] parts = input.split("\\+");
+            return Integer.parseInt(parts[0]) + Integer.parseInt(parts[1]);
+        }
         return Integer.parseInt(input);
     }
 }
--- src/test/java/com/amalvadkar/sck/StringCalculatorTest.java
+++ src/test/java/com/amalvadkar/sck/StringCalculatorTest.java
@@ -2,6 +2,7 @@ package com.amalvadkar.sck;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorTest {
@@ -13,5 +14,14 @@ public class StringCalculatorTest {
         assertThat(stringCalculator.execute("1")).isEqualTo(1);
         assertThat(stringCalculator.execute("456")).isEqualTo(456);
         assertThat(stringCalculator.execute("-2")).isEqualTo(-2);
+    }
+
+    @Test
+    void should_return_sum_if_two_numbers_are_passed_as_input_separated_by_plus_operator() {
+        StringCalculator stringCalculator = new StringCalculator();
+
+        assertThat(stringCalculator.execute("1+1")).isEqualTo(2);
+        assertThat(stringCalculator.execute("57+100")).isEqualTo(157);
+        assertThat(stringCalculator.execute("1000+0")).isEqualTo(1000);
     }
 }
