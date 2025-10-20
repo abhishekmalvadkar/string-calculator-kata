9ee86d7df391ce7a241a6e523293a7da334681ff
Abhishek <abhi@dev.com>
2025-10-20 08:40:20 +0530

    test-reactor : should_return_number_as_it_is_if_input_is_single_number

--- src/test/java/com/amalvadkar/sck/StringCalculatorTest.java
+++ src/test/java/com/amalvadkar/sck/StringCalculatorTest.java
@@ -1,6 +1,8 @@
 package com.amalvadkar.sck;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,12 +18,15 @@ public class StringCalculatorTest {
         assertThat(result).isEqualTo("0");
     }
 
-    @Test
-    void should_return_number_as_it_is_if_input_is_single_number() {
+    @ParameterizedTest
+    @CsvSource({
+            "1,1",
+            "12,12",
+            "123,123"
+    })
+    void should_return_number_as_it_is_if_input_is_single_number(String numbers, String actual) {
         StringCalculator stringCalculator = new StringCalculator();
 
-        assertThat(stringCalculator.add("1")).isEqualTo("1");
-        assertThat(stringCalculator.add("12")).isEqualTo("12");
-        assertThat(stringCalculator.add("123")).isEqualTo("123");
+        assertThat(stringCalculator.add(numbers)).isEqualTo(actual);
     }
 }
