c6558709b5f72daca727c9925fe6cf8b82805a69
Abhishek <abhi@dev.com>
2025-10-20 08:30:25 +0530

    refactor : should_return_zero_if_input_is_empty_string

--- src/main/java/com/amalvadkar/sck/StringCalculator.java
+++ src/main/java/com/amalvadkar/sck/StringCalculator.java
@@ -1,10 +1,11 @@
 package com.amalvadkar.sck;
 
 public class StringCalculator {
+
+    public static final String ZERO = "0";
+
     public String add(String numbers) {
-        if (numbers.isEmpty()) {
-            return "0";
-        }
+        if (numbers.isEmpty()) return ZERO;
         return null;
     }
 }
