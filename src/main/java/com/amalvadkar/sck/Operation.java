20237780678f1a7b75c7ea32c8cbd5c7742b234b
Abhishek <abhi@dev.com>
2025-10-25 14:38:56 +0530

    Just refactoring

--- src/main/java/com/amalvadkar/sck/Operation.java
+++ src/main/java/com/amalvadkar/sck/Operation.java
@@ -1,6 +1,42 @@
 package com.amalvadkar.sck;
 
+import java.math.BigDecimal;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import static com.amalvadkar.sck.Splitter.split;
+
 public enum Operation {
-    ADD,
-    MULTIPLY
+    ADD(Operation::add),
+    MULTIPLY(Operation::multiply);
+
+    private final Function<String, String> operation;
+
+    Operation(Function<String, String> operation) {
+        this.operation = operation;
+    }
+
+    public String performOn(String numbers){
+        return operation.apply(numbers);
+    }
+
+    public static String add(String numbers) {
+        return String.valueOf(add(split(numbers)));
+    }
+
+    public static BigDecimal add(String[] numbers) {
+        return Stream.of(numbers)
+                .map(BigDecimal::new)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    public static String multiply(String numbers) {
+        return String.valueOf(multiply(split(numbers)));
+    }
+
+    public static BigDecimal multiply(String[] numbers) {
+        return Stream.of(numbers)
+                .map(BigDecimal::new)
+                .reduce(BigDecimal.ONE, BigDecimal::multiply);
+    }
 }
--- src/main/java/com/amalvadkar/sck/StringCalculator.java
+++ src/main/java/com/amalvadkar/sck/StringCalculator.java
@@ -1,12 +1,7 @@
 package com.amalvadkar.sck;
 
-import java.math.BigDecimal;
-import java.util.stream.Stream;
-
-import static com.amalvadkar.sck.Splitter.split;
 import static com.amalvadkar.sck.Utility.isEmptyOrNull;
 import static com.amalvadkar.sck.Validator.validate;
-import static java.lang.String.valueOf;
 
 public class StringCalculator {
 
@@ -25,34 +20,11 @@ public class StringCalculator {
         String error = validate(numbers);
         if (numbersHaveValidation(error)) return error;
 
-        return switch (operation) {
-            case ADD -> sum(numbers);
-            case MULTIPLY -> multiplication(numbers);
-        };
-    }
-
-    private static String sum(String numbers) {
-        return valueOf(sum(split(numbers)));
+        return operation.performOn(numbers);
     }
 
     private static boolean numbersHaveValidation(String error) {
         return !error.isEmpty();
-    }
-
-    private static String multiplication(String numbers) {
-        return valueOf(multiply(split(numbers)));
-    }
-
-    private static BigDecimal sum(String[] numbers) {
-        return Stream.of(numbers)
-                .map(BigDecimal::new)
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
-    }
-
-    private static BigDecimal multiply(String[] numbers) {
-        return Stream.of(numbers)
-                .map(BigDecimal::new)
-                .reduce(BigDecimal.ONE, BigDecimal::multiply);
     }
 
     private static boolean hasSingleNumberWithoutSeparator(String numbers) {
